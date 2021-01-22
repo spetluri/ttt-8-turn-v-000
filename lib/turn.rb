87a953d54b57e714772c3bb103b12ad07bc15c10
Sai Petluri <spetluri.1@gmail.com>
2021-01-22 04:23:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -44,8 +44,6 @@ end
 def position_taken?(board, position)
   if (board[position] == "" || board[position] == " " || board[position] == nil)
     false
-  elsif (board[position] == "X" || board[position] == "O")
-    true
   else
     true
   end
